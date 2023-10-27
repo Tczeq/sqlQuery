19bc1000d0a9c0dc4d8f5de116f23d6c0e4780b4
Tczeq <tczekanowski@gmail.com>
2023-10-27 22:42:56 +0200

    poprawiono pociagi

--- src/main/java/pracaDomowaTransakcje/service/TrainService.java
+++ src/main/java/pracaDomowaTransakcje/service/TrainService.java
@@ -14,19 +14,23 @@ public class TrainService {
 
     public void transactionTrain() throws SQLException {
 
-        try{
+        try {
             connection.setAutoCommit(false);
 
             // DELETE
             PreparedStatement prepStm1 = null;
 
-            // TO POD SPODEM TO TRANSAKCJA
             try {
                 prepStm1 = connection.prepareStatement("delete from train where name = ?");
-                prepStm1.setString(1, "Przemek");
+                prepStm1.setString(1, "Martyna");
 
                 int deletedRows = prepStm1.executeUpdate();
                 System.out.println("Usunieto: " + deletedRows);
+                if (deletedRows == 0) {
+                    connection.rollback();
+                    System.out.println("Operacja DELETE nie powiodla sie. Wykonuje rollback");
+                    return;
+                }
             } finally {
                 if (prepStm1 != null) prepStm1.close();
             }
@@ -35,16 +39,35 @@ public class TrainService {
             PreparedStatement prepStm2 = null;
             try {
                 prepStm2 = connection.prepareStatement("insert into train (name, length, isWars, seatNumber) values (?, ?, ?, ?);");
-                prepStm2.setString(1, "Justyna");
-                prepStm2.setDouble(2, 2000.0);
+                prepStm2.setString(1, "Janusz");
+                prepStm2.setDouble(2, 213.02);
                 prepStm2.setBoolean(3, true);
-                prepStm2.setInt(4, 200);
-
+                prepStm2.setInt(4, 2132);
 
                 int insertedRows = prepStm2.executeUpdate();
                 System.out.println("Dodano: " + insertedRows);
             } finally {
                 if (prepStm2 != null) prepStm2.close();
+            }
+
+            // UPDATE
+            PreparedStatement prepStm3 = null;
+            try {
+                prepStm3 = connection.prepareStatement("UPDATE train SET name = ? WHERE name = ?");
+                // Nowa imie
+                prepStm3.setString(1, "Justyna");
+                //Istniejace imie
+                prepStm3.setString(2, "Justa");
+
+                int updatedRows = prepStm3.executeUpdate();
+                System.out.println("Zaktualizowano: " + updatedRows);
+                if (updatedRows == 0) {
+                    connection.rollback();
+                    System.out.println("Operacja UPDATE nie powiodla sie. Wykonuje rollback");
+                    return;
+                }
+            } finally {
+                if (prepStm3 != null) prepStm3.close();
             }
 
             connection.commit();
